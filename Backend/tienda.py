3353166689e76f19f4b4368fac74b63447c919c4
daiana <daladrey@fi.uba.ar>
2024-06-24 15:12:58 -0300

    conexion base de datos con backend y frontend

--- Backend/tienda.py
+++ Backend/tienda.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app) #donde esta el entorno virtual intalar -> pip install flask-cors para que funcione todo 
 
 port = 5000
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:1215308@localhost:5432/tienda_online' #conecto la base de datos
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:1215308@localhost:5432/tienda_online' #conecto la base de datos //usario_bd:contraseña@localhost:5432/nombre_bd
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.route('/')
