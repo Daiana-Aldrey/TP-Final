636b4b91cce1a05f76b70d34613f799e0e25cbba
JulianCasazza <jcasazza@fi.uba.ar>
2024-07-03 11:39:27 -0300

    Funcionalidad de redirección de los logos a las páginas de las marcas

--- Backend/tienda.py
+++ Backend/tienda.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS 
-from modelos import db, Celular, Plan
+from modelos import db, Celular,Tablet, Notebook, Plan
+from filtrar_productos import filtrar_productos_por_marca, filtrar_notebooks_por_marca
+
+SAMSUNG = "Samsung"
+APPLE = "Apple"
+XIAOMI = "Xiaomi"
 
 app = Flask(__name__)
 CORS(app) #donde esta el entorno virtual intalar -> pip install flask-cors para que funcione todo 
@@ -9,9 +14,46 @@ port = 5000
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:postgres@localhost:5432/tienda_online' #conecto la base de datos //usario_bd:contraseña@localhost:5432/nombre_bd
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
+
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route("/samsung/celulares/")
+def celulares_samsung():
+    return filtrar_productos_por_marca(Celular, SAMSUNG)
+
+@app.route("/samsung/tablets/")
+def tablets_samsung():
+    return filtrar_productos_por_marca(Tablet, SAMSUNG)
+
+@app.route("/samsung/notebooks/")
+def notebooks_samsung():
+    return filtrar_notebooks_por_marca(SAMSUNG)    
+
+@app.route("/apple/celulares/")
+def celulares_apple():
+    return filtrar_productos_por_marca(Celular, APPLE)
+
+@app.route("/apple/tablets/")
+def tablets_apple():
+    return filtrar_productos_por_marca(Tablet, APPLE)
+
+@app.route("/apple/notebooks/")
+def notebooks_apple():
+    return filtrar_notebooks_por_marca(APPLE)    
+
+@app.route("/xiaomi/celulares/")
+def celulares_sxiaomi():
+    return filtrar_productos_por_marca(Celular, XIAOMI)
+
+@app.route("/xiaomi/tablets/")
+def tablets_xiaomi():
+    return filtrar_productos_por_marca(Tablet, XIAOMI)
+
+@app.route("/xiaomi/notebooks/")
+def notebooks_xiaomi():
+    return filtrar_notebooks_por_marca(XIAOMI)     
 
 @app.route("/celulares/") #muestra todos los celulares
 def celulares():
@@ -56,12 +98,14 @@ def celular(id_celular):
             'memoria' : celular.memoria,
             'camara delantera': celular.camara_delantera,
             'camara trasera' : celular.camara_trasera,
-            'bateria' : celular.baterias,
+            'bateria' : celular.bateria,
+            'pantalla' : celular.pantalla,
             'precio': celular.precio,
-            'plan de financiacion': celular.financiamiento,
+            'plan de financiamiento': celular.financiacion,
             'descripcion': celular.descripcion,
             'imagen': celular.imagen_url
         }
+
         return jsonify(celular_informacion)
     except:
         return jsonify("Lo buscado no es un producto que este a la venta"), 400
