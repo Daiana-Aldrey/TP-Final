535ee996502fb0d88c16ea51eb3c20d91951e0e5
juan david chaparro <davidacosta312002@gmail.com>
2024-07-19 11:12:47 -0300

    scripts cambiados de carpeta

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -1,7 +1,24 @@
+function mostrarFormulario(categoria) {
+    document.getElementById('seleccion-inicial').style.display = 'none';
+    document.getElementById('formulario-compra').style.display = 'block';
+
+    fetch(`http://localhost:5000/categorias/${categoria}`)
+        .then(response => response.json())
+        .then(data => {
+            const marcaSelect = document.getElementById('marca');
+            marcaSelect.innerHTML = '';
+            data.marcas.forEach(marca => {
+                const option = document.createElement('option');
+                option.value = marca;
+                option.textContent = marca;
+                marcaSelect.appendChild(option);
+            });
+        })
+        .catch(error => console.error('Error al obtener marcas:', error));
+}
+
 function mostrarMensaje() {
     document.getElementById('mensaje-exito').style.display = 'block';
-
-    document.getElementById('vender_form').reset();
-
+    document.getElementById('comprar_form').reset();
     return false;
 }
